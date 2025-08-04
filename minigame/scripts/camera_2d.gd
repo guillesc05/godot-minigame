f2d254fa67b740bd34f82956104f80228d3b3754
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-08-04 16:22:55 +0200

    mountain relic

--- minigame/scripts/camera_2d.gd
+++ minigame/scripts/camera_2d.gd
@@ -1,6 +1,8 @@
 extends Camera2D
 @onready var player: CharacterBody2D = $"../Player"
 
+var camYOffset = -30
+
 func _process(delta: float) -> void:
 	position.x = player.position.x
-	position.y = player.position.y
+	position.y = lerp(position.y, player.position.y + camYOffset, 0.01)
--- minigame/scripts/terrain_generator.gd
+++ minigame/scripts/terrain_generator.gd
@@ -3,8 +3,8 @@ extends TileMapLayer
 
 
 const MAX_TERRAIN_HEIGHT: int = 120
-const MIN_TERRAIN_HEIGHT: int = 2
 const MOUNTAIN_WIDTH: int = 200
+const CANNON_BASE_WIDTH = 10
 var rng = RandomNumberGenerator.new()
 
 #genera en $ores las posiciones donde se deberian encontrar las menas, crea el objeto en la escena y emite el objeto por signal
@@ -13,6 +13,10 @@ func _ready() -> void:
 		
 
 func generate_terrain()->void:
+	for k in range(-CANNON_BASE_WIDTH, CANNON_BASE_WIDTH):
+		set_cell(Vector2i(k, 0), 0, Vector2i(1,0))
+		for k2 in 4:
+			set_cell(Vector2i(k, k2+ 1), 0, Vector2i(1,1))
 	var noise = FastNoiseLite.new()
 	noise.frequency = 1
 	var rnd_seed = rng.randi_range(0, 10000)
@@ -20,11 +24,11 @@ func generate_terrain()->void:
 	for i in range(0, MOUNTAIN_WIDTH):
 		var value:float = float(i) / (MOUNTAIN_WIDTH)
 		var rad = lerp(0.0, PI/2, value)
-		var height = round(sin(rad) * MAX_TERRAIN_HEIGHT)
+		var height =round(sin(rad) * MAX_TERRAIN_HEIGHT)
 		
-		var heightNoise = round(noise.get_noise_1d(rnd_seed + i) * 2)
+		var heightNoise = round(noise.get_noise_1d(rnd_seed + i) * 1.5)
 		height += heightNoise
 		
-		set_cell(Vector2i(i, -height), 0, Vector2i(1,0))
-		for j in height:
-			set_cell(Vector2i(i, -j), 0, Vector2i(1,1))
+		set_cell(Vector2i(i + CANNON_BASE_WIDTH, -height), 0, Vector2i(1,0))
+		for j in (height):
+			set_cell(Vector2i(i + CANNON_BASE_WIDTH, -j), 0, Vector2i(1,1))
