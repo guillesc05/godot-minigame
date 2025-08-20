b86c43a1dd03460e8b7a4b97be6fe686212d2376
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-08-20 20:15:00 +0200

    build

--- minigame/scripts/canvas.gd
+++ minigame/scripts/canvas.gd
@@ -1,12 +1,14 @@
 extends Sprite2D
 @onready var camera: Camera2D = $"../../Camera2D"
-@onready var me = $"."
+
+@onready var falling_audio:AudioStreamPlayer2D = $falling_audio
 
 const MIN_VELOCITY:float = -.5
-const MAX_VELOCITY:float = -4000
+const MAX_VELOCITY:float = -3000
 
 var currVelY = MIN_VELOCITY
 var tween:Tween
+var audioTween:Tween
 
 func _ready() -> void:
 	SceneInfo.dialogue_ended.connect(on_dialogue_end)
@@ -20,8 +22,13 @@ func _process(delta: float) -> void:
 func on_game_won():
 	await get_tree().create_timer(3.5).timeout
 	tween.pause()
+	audioTween.pause()
+	falling_audio.stop()
 	currVelY = 0
 
 func on_dialogue_end():
+	falling_audio.play()
+	audioTween = create_tween().set_trans(Tween.TRANS_EXPO)
 	tween = create_tween().set_trans(Tween.TRANS_EXPO)
+	audioTween.tween_property(falling_audio, "volume_db", 0.0, SceneInfo.TIME_UNTIL_LOST)
 	tween.tween_property(self,"currVelY", MAX_VELOCITY, SceneInfo.TIME_UNTIL_LOST)
